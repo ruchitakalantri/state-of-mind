aad48d832e8bda233c25bf5054ccdac3e8acbaf8
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2020-07-12 13:12:28 -0700

    create xcode project

---
+++ State Of Mind/State Of Mind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  State Of Mind
+//
+//  Created by Ruchita Rathi on 7/12/20.
+//  Copyright © 2020 Updatus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+        override func viewDidLoad() {
+                super.viewDidLoad()
+                // Do any additional setup after loading the view.
+        }
+
+
+}
+
